e58e9d635d4573beda03deb87a046834f0c67f58
tsubasamusu <rdr2daisuki@gmail.com>
2022-09-03 15:01:22 +0900

    UI用のゲームオブジェクトを全て作成した。

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;//UIを使用
 
 public class UIManager : MonoBehaviour
 {
-   
+    [SerializeField]
+    private Image imgBackGround;//背景
+
+    [SerializeField]
+    private Image imgLogo;//ロゴ
+
+    [SerializeField]
+    private Text txtTime;//経過時間
+
+    private float timer;//経過時間計測用
 }
