932bf27426c07bdc3fa5cbf23e42f355b9369d00
tsubasamusu <rdr2daisuki@gmail.com>
2022-09-03 14:46:55 +0900

    「cameraController.cs」を修正し、カメラを滑らかに動かせるようにした。

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,9 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private Transform targetTran;//対象物
 
+    [SerializeField]
+    private float smooth;//視点移動の滑らかさ
+
     private Vector3 firstPos;//カメラの初期位置
 
     /// <summary>
@@ -19,11 +22,14 @@ public class CameraController : MonoBehaviour
     }
 
     /// <summary>
-    /// 毎フレーム呼び出される
+    /// 一定時間ごとに呼び出される
     /// </summary>
-    private void Update()
+    private void FixedUpdate()
     {
-        //カメラの位置を更新
-        transform.position = new Vector3(firstPos.x,firstPos.y,targetTran.position.z);
+        //更新すべき位置を取得
+        Vector3 pos = new Vector3(firstPos.x,firstPos.y,targetTran.position.z);
+
+        //カメラを滑らかに移動させる
+        transform.position = Vector3.Lerp(transform.position, pos, Time.fixedDeltaTime * smooth);
     }
 }
