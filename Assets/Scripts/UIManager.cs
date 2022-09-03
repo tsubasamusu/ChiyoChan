8c2a9ceeeb730451215824b3bb5c7829889cfe45
tsubasamusu <rdr2daisuki@gmail.com>
2022-09-03 15:08:01 +0900

    ロゴを2つインポートし、設定した。

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,13 +6,22 @@ using UnityEngine.UI;//UIを使用
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
-    private Image imgBackGround;//背景
+    private Image imgBackground;//背景
 
     [SerializeField]
     private Image imgLogo;//ロゴ
 
     [SerializeField]
     private Text txtTime;//経過時間
+
+    [SerializeField]
+    private Text txtLength;//ゴールまでの距離
+
+    [SerializeField]
+    private Sprite sprGameStart;//ゲームスタートロゴ
+
+    [SerializeField]
+    private Sprite sprGameClear;//ゲームクリアロゴ
 
     private float timer;//経過時間計測用
 }
